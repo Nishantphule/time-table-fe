8c2375614add4fed8e685d1f2d6a154623cbb9f6
Nishant Phule <nishantphule12@gmail.com>
2024-06-17 18:10:46 +0530

    course wise done

--- src/Components/Forms/CourseWise.jsx
+++ src/Components/Forms/CourseWise.jsx
@@ -1,164 +1,205 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CourseWise = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [courseCodes, setCourseCodes] = useState([1, 2, 3, 4]);
-    const [allYearSemesters, setAllYearSemesters] = useState([]);
-    const [allMasterCodes, setAllMasterCodes] = useState([]);
+  const [courseCodes, setCourseCodes] = useState([]);
+  const [allYearSemesters, setAllYearSemesters] = useState([]);
+  const [allMasterCodes, setAllMasterCodes] = useState([]);
 
-    // const [filteredYearSemesters, setFilteredYearSemesters] = useState([]);
-    // const [filteredMasterCodes, setFilteredMasterCodes] = useState([]);
+  // const [filteredYearSemesters, setFilteredYearSemesters] = useState([]);
+  // const [filteredMasterCodes, setFilteredMasterCodes] = useState([]);
 
-    const [selectedCourseCode, setSelectedCourseCode] = useState('');
-    const [selectedYearSemester, setSelectedYearSemester] = useState('');
-    const [selectedMasterCode, setSelectedMasterCode] = useState('');
+  const [selectedCourseCode, setSelectedCourseCode] = useState("");
+  const [selectedYearSemester, setSelectedYearSemester] = useState("");
+  const [selectedMasterCode, setSelectedMasterCode] = useState("");
 
-    const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-    useEffect(() => {
-        // Fetch all data from the backend when the component mounts
-        axios.get('http://localhost:3001/coursewise/coursecode')
-            .then(response => setCourseCodes([...response.data].map(data=>{
-                return {code:data.course_code,name:data.course_name}})))
-            .catch(error => console.error('Error fetching course codes:', error));
-    }, []);
+  useEffect(() => {
+    // Fetch all data from the backend when the component mounts
+    axios
+      .get("http://localhost:3001/coursewise/coursecode")
+      .then((response) =>
+        setCourseCodes(
+          [...response.data].map((data) => {
+            return { code: data.course_code, name: data.course_name };
+          })
+        )
+      )
+      .catch((error) => console.error("Error fetching course codes:", error));
+  }, []);
 
-    useEffect(() => {
-        // Filter year/semesters based on selected course code
-        if (selectedCourseCode) {
-            axios.get(`http://localhost:3001/coursewise/yearcode/${selectedCourseCode}`)
-            .then(response => setAllYearSemesters(response.data))
-            .catch(error => console.error('Error fetching year/semesters:', error));
-        } else {
-            setSelectedYearSemester('');
-            setSelectedMasterCode('');
-        }
-    }, [selectedCourseCode, allYearSemesters]);
+  useEffect(() => {
+    // Filter year/semesters based on selected course code
+    if (selectedCourseCode) {
+      axios
+        .get(`http://localhost:3001/coursewise/yearcode/${selectedCourseCode}`)
+        .then((response) => setAllYearSemesters(response.data))
+        .catch((error) =>
+          console.error("Error fetching year/semesters:", error)
+        );
+    } else {
+      setSelectedYearSemester("");
+      setSelectedMasterCode("");
+    }
+  }, [selectedCourseCode, allYearSemesters]);
 
-    useEffect(() => {
-        // Filter master codes based on selected course code and year/semester
-        if (selectedCourseCode && selectedYearSemester) {
-            axios.get(`http://localhost:3001/coursewise/mastercode/${selectedCourseCode}/${selectedYearSemester}`)
-            .then(response => setAllMasterCodes(response.data))
-            .catch(error => console.error('Error fetching master codes:', error));
-        } else {
-            setSelectedMasterCode('');
-        }
-    }, [selectedCourseCode, selectedYearSemester]);
+  useEffect(() => {
+    // Filter master codes based on selected course code and year/semester
+    if (selectedCourseCode && selectedYearSemester) {
+      axios
+        .get(
+          `http://localhost:3001/coursewise/mastercode/${selectedCourseCode}/${selectedYearSemester}`
+        )
+        .then((response) => setAllMasterCodes(response.data))
+        .catch((error) => console.error("Error fetching master codes:", error));
+    } else {
+      setSelectedMasterCode("");
+    }
+  }, [selectedCourseCode, selectedYearSemester]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!selectedCourseCode || !selectedYearSemester || !selectedMasterCode) {
-            setError('Please select a value from all dropdowns.');
-        } else {
-            setError('');
-            // Handle form submission logic here
-            navigate(`/timetable?filter=Coursewise&coursecode=${selectedCourseCode}&yearsemester=${selectedYearSemester}&mastercode=${selectedMasterCode}`);
-            console.log('Form submitted with:', {
-                selectedCourseCode,
-                selectedYearSemester,
-                selectedMasterCode,
-            });
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!selectedCourseCode || !selectedYearSemester || !selectedMasterCode) {
+      setError("Please select a value from all dropdowns.");
+    } else {
+      setError("");
+      // Handle form submission logic here
+      const name = [...courseCodes].filter(
+        (course) => course.code === selectedCourseCode
+      );
+      navigate(
+        `/coursewise?coursecode=${selectedCourseCode}&name=${name[0].name}&yearsemester=${selectedYearSemester}&mastercode=${selectedMasterCode}`
+      );
+      console.log("Form submitted with:", {
+        selectedCourseCode,
+        selectedYearSemester,
+        selectedMasterCode,
+      });
+    }
+  };
+  return (
+    <div className="col-12 col-md-6 col-lg-4">
+      <div className="form_box" style={{ border: "5px solid #b0a9ea" }}>
+        <div
+          className="form_title"
+          style={{ background: "#d0cdeb", color: "#555086" }}
+        >
+          MSBTE Course-wise Summer 2024
+          <span style={{ color: "red" }}> FINAL </span>
+          Time Table for Theory Examination
+        </div>
+        <div className="p-2">
+          <form onSubmit={handleSubmit} className="form-table">
+            <table className="table">
+              <tbody>
+                <tr>
+                  <td className="label-cell">
+                    <label htmlFor="course-code-select" className="label">
+                      Course Code:
+                    </label>
+                  </td>
+                  <td>
+                    <select
+                      id="course-code-select"
+                      name="courseCodes"
+                      value={selectedCourseCode}
+                      onChange={(e) => {
+                        setSelectedCourseCode(e.target.value);
+                      }}
+                      className="select"
+                    >
+                      <option value="">--Select--</option>
+                      {courseCodes.map((course, i) => {
+                        if (course.code !== "--") {
+                          return (
+                            <option
+                              style={{ fontSize: "14px" }}
+                              key={i}
+                              value={course.code}
+                            >
+                              {course.code} - {course.name}
+                            </option>
+                          );
+                        } else {
+                          return "";
+                        }
+                      })}
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="label-cell">
+                    <label htmlFor="year-semester-select" className="label">
+                      Year/Semester:
+                    </label>
+                  </td>
+                  <td>
+                    <select
+                      id="year-semester-select"
+                      name="yearSemesters"
+                      value={selectedYearSemester}
+                      onChange={(e) => setSelectedYearSemester(e.target.value)}
+                      className="select"
+                      disabled={!selectedCourseCode}
+                    >
+                      <option value="">--Select--</option>
+                      {allYearSemesters.map((yearSemester, i) => (
+                        <option
+                          style={{ fontSize: "14px" }}
+                          key={i}
+                          value={yearSemester.year_code}
+                        >
+                          {yearSemester.year_code}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="label-cell">
+                    <label htmlFor="master-code-select" className="label">
+                      Master Code:
+                    </label>
+                  </td>
+                  <td>
+                    <select
+                      id="master-code-select"
+                      name="masterCodes"
+                      value={selectedMasterCode}
+                      onChange={(e) => setSelectedMasterCode(e.target.value)}
+                      className="select"
+                      disabled={!selectedYearSemester}
+                    >
+                      <option value="">--Select--</option>
+                      {allMasterCodes.map((data, i) => (
+                        <option key={i} value={data.master_code}>
+                          {data.master_code}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="2" style={{ textAlign: "center" }}>
+                    <button type="submit" className="button">
+                      Time Table
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            {error && (
+              <p style={{ color: "red", textAlign: "center" }}>{error}</p>
+            )}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='col-12 col-md-6 col-lg-4'>
-            <div className="form_box" style={{ border: "5px solid #b0a9ea" }}>
-                <div className="form_title" style={{ background: "#d0cdeb", color: "#555086" }}>
-                    MSBTE Course-wise Summer 2024
-                    <span style={{ color: "red" }}>
-                        {" "}FINAL{" "}
-                    </span>
-                    Time Table for Theory Examination
-                </div>
-                <div className='p-2'>
-                    <form onSubmit={handleSubmit} className="form-table">
-                        <table className='table'>
-                            <tbody>
-                                <tr>
-                                    <td className="label-cell">
-                                        <label htmlFor="course-code-select" className="label">Course Code:</label>
-                                    </td>
-                                    <td>
-                                        <select
-                                            id="course-code-select"
-                                            name="courseCodes"
-                                            value={selectedCourseCode}
-                                            onChange={(e) => setSelectedCourseCode(e.target.value)}
-                                            className="select"
-                                        >
-                                            <option value="">--Select--</option>
-                                            {courseCodes.map((course, i) => {
-                                                if(course.code!=="--"){
-                                                return(
-                                                    <option style={{fontSize:"14px"}} key={i} value={course.code}>
-                                                    {course.code} - {course.name}
-                                                </option>
-                                            )}})}
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="label-cell">
-                                        <label htmlFor="year-semester-select" className="label">Year/Semester:</label>
-                                    </td>
-                                    <td>
-                                        <select
-                                            id="year-semester-select"
-                                            name="yearSemesters"
-                                            value={selectedYearSemester}
-                                            onChange={(e) => setSelectedYearSemester(e.target.value)}
-                                            className="select"
-                                            disabled={!selectedCourseCode}
-                                        >
-                                            <option value="">--Select--</option>
-                                            {allYearSemesters.map((yearSemester, i) => (
-                                                <option style={{fontSize:"14px"}} key={i} value={yearSemester.year_code}>
-                                                    {yearSemester.year_code}
-                                                </option>
-                                            ))}
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="label-cell">
-                                        <label htmlFor="master-code-select" className="label">Master Code:</label>
-                                    </td>
-                                    <td>
-                                        <select
-                                            id="master-code-select"
-                                            name="masterCodes"
-                                            value={selectedMasterCode}
-                                            onChange={(e) => setSelectedMasterCode(e.target.value)}
-                                            className="select"
-                                            disabled={!selectedYearSemester}
-                                        >
-                                            <option value="">--Select--</option>
-                                            {allMasterCodes.map((data, i) => (
-                                                <option key={i} value={data.master_code}>
-                                                    {data.master_code}
-                                                </option>
-                                            ))}
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="2" style={{ textAlign: 'center' }}>
-                                        <button type="submit" className="button">Time Table</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
-                    </form>
-                </div>
-            </div>
-        </div >
-    )
-}
-
-export default CourseWise
+export default CourseWise;
---
+++ src/Components/Timetables/CoursewiseTT.jsx
@@ -0,0 +1,141 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const CoursewiseTT = () => {
+  const location = useLocation();
+  const [schema, setSchema] = useState({
+    course_name: "",
+    course_code: "",
+    year_code: 0,
+    master_code: "",
+  });
+  const [day, setDays] = useState([]);
+  const [timetable, setTimetable] = useState([]);
+
+  useEffect(() => {
+    // Access query parameters
+    const searchParams = new URLSearchParams(location.search);
+
+    async function getTimetableData() {
+      const course_code = searchParams.get("coursecode");
+      const course_name = searchParams.get("name");
+      const year_code = searchParams.get("yearsemester");
+      const master_code = searchParams.get("mastercode");
+      setSchema({ course_name, course_code, year_code, master_code });
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/coursewise/dayquery/${course_code}/${year_code}/${master_code}`
+        );
+        setDays(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching master codes:", error);
+      }
+    }
+    getTimetableData();
+  }, [location.search]);
+
+  useEffect(() => {
+    async function fetchTimetable() {
+      if (day.length === 0) return;
+
+      try {
+        const timetablePromises = day.map((d) =>
+          axios.get(
+            `http://localhost:3001/coursewise/timetable/${schema.course_code}/${schema.year_code}/${schema.master_code}/${d.exam_dayw}/${d.daysession}`
+          )
+        );
+
+        const responses = await Promise.all(timetablePromises);
+        const timetableData = responses.map((response) => response.data);
+        setTimetable(timetableData);
+      } catch (error) {
+        console.error("Error fetching timetable data:", error);
+      }
+    }
+
+    fetchTimetable();
+  }, [day, schema.course_code, schema.year_code, schema.master_code]);
+
+  const navigate = useNavigate();
+
+  const headingStyle = { backgroundColor: "#cad9f1", color: "#506a9e" };
+  return (
+    <div>
+      <h2
+        style={{
+          padding: "5px 0px",
+          textAlign: "center",
+          fontSize: "14px",
+          fontWeight: "bold",
+        }}
+      >
+        Course-Wise Time Table for Summer 2024 Theory Examination
+      </h2>
+
+      <table className="table table-bordered" width="100%">
+        <tbody>
+          <tr>
+            <th colSpan="4" style={{ color: "#506a9e", textAlign: "center" }}>
+              {`Course:${schema.course_code}(${schema.course_name})  Year/Semester:${schema.year_code}   Master:${schema.master_code}`}
+              <button onClick={() => navigate("/")}>
+                Click here to go Back
+              </button>
+            </th>
+          </tr>
+          {timetable.length > 0 ? (
+            timetable.map((table, i) => {
+              return table.map((data) => {
+                return (
+                  <>
+                    <tr>
+                      <th
+                        colSpan="4"
+                        style={{ color: "#800000", textAlign: "center" }}
+                      >
+                        Date: {data.date}{" "}
+                        {data.daysession === "A" ? "Afternoon" : "Morning"} Day:
+                        {data.exam_dayw}
+                      </th>
+                    </tr>
+                    <tr>
+                      <th style={headingStyle}>Time Slot</th>
+                      <th style={headingStyle}>Subject Code</th>
+                      <th style={headingStyle}>Subject Name</th>
+                      <th style={headingStyle}>Schema</th>
+                    </tr>
+                    <tr>
+                      <th>{data.paper_time}</th>
+                      <th>{data.paper_code}</th>
+                      <th>{data.subject_name}</th>
+                      <th>
+                        {schema.course_code} - {schema.year_code} -{" "}
+                        {schema.master_code}
+                      </th>
+                    </tr>
+                  </>
+                );
+              });
+            })
+          ) : (
+            <h2
+              style={{
+                padding: "5px 0px",
+                textAlign: "center",
+                fontSize: "16px",
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              No Data to Display
+            </h2>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CoursewiseTT;
